e18e5d13ae041c92b23d9fe69a36ca2c64c9c97e
Erick Lui <erickjin.lui@gmail.com>
2017-08-16 13:25:50 -0700

    adjusted scaling of portfolio items

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 var express = require('express'),
-  bodyParser = require('body-parser'),
   port = 8000,
   app = express();
-// app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
   res.render('index');
